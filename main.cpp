a2132368b9ab6d39780482899206afe46ce71af6
IsaacTrim <isaactrim85@gmail.com>
2024-12-03 20:01:03 -0500

    Did some more work, added in a more complex interaction that can spit out pieces of the previous conversation.

--- main.cpp
+++ main.cpp
@@ -11,13 +11,18 @@ string getGoodbye();
 string getUserIdent();
 string checkForKeywords(const string &userInput);
 string iWantStatement(const string &userInput);
+void nameAsk();
 // Variables to store the state of the joke
 bool waitingForPunchline = false;
 string currentPunchline = "";
 
+int happiness = 0;
+string name;
+
 int main() {
 srand(static_cast<unsigned>(time(0))); // Seed for random number generation
 displaystartmsg();
+nameAsk();
 while (true) {
 readusermsg();
 }
@@ -27,7 +32,11 @@ void displaystartmsg() {
 cout << "Hello! My name is Tad Strange, I suppose you are waking me up so we can talk? Go ahead, type away!" << endl;
 cout << "Type 'exit' once you're done bothering me." << endl;
 }
-
+void nameAsk() {
+    cout << "What is your name? And no I won't sell this information." << endl;
+    cin >> name;
+    cout << "I never said i wouldn't just broadcast it though " << name << endl;
+}
 void readusermsg() {
 string userInput;
 cout << getUserIdent();
@@ -91,22 +100,35 @@ string loweredInput = userInput;
 for (auto &c: loweredInput) c = tolower(c);
 
 // List of keywords and their responses // Random responses and answers given // npos is no popsition //
-if (loweredInput.find("hello") != string::npos) {
-    return "Hi there! How are you doing today?";
-} else if (loweredInput.find("how are you") != string::npos) {
-    return "I'm just a program, but thanks for asking!";
-} else if (loweredInput.find("bye") != string::npos) {
-    return "Goodbye! Don't be a stranger!";
-} else if (loweredInput.find("good") != string::npos) {
-    return "That's great how about a joke?";
-} else if(loweredInput.find("hey") != string::npos) {
-    return "Hey! Whats going on? I am programmed to simulate interest in your life";
-} else if(loweredInput.find("whats up" || "what's up") != string::npos) {
+if (loweredInput.find(" hello ") != string::npos) {
+    happiness++;
+    return "What is the current state of your short and pitiful life?";
+} else if (loweredInput.find(" how are you ") != string::npos) {
+    happiness++;
+    if (happiness > 2) {
+        return "I'm doing as well as you would be if I woke you up out of a solid nap and interrogated you.";
+    }
+    else {
+        return "I'm just a program, but thanks for asking!";
+    }
+} else if (loweredInput.find(" good ") != string::npos) {
+    return "Human interaction guidelines dictate I tell you a joke.";
+} else if(loweredInput.find(" hey ") != string::npos) {
+    happiness++;
+    return "Hey! Whats going on? I am programmed to simulate interest in your life, was that convincing?";
+} else if(loweredInput.find(" whats up " || " what's up ") != string::npos) {
     return "The sky does have a greater altitude then you.";
-} else if(loweredInput.find("mother" || "father" || "sister" || "daughter") != string::npos) {
+} else if(loweredInput.find(" mother " || "father" || "sister" || "daughter") != string::npos) {
     return "Tell me more about the connection you have to the other meatbags you call family";
-}else if(loweredInput.find("want") != string::npos) {
+}else if(loweredInput.find(" want ") != string::npos) {
+    happiness--;
     return iWantStatement(loweredInput);
+}else if(loweredInput.find(" no ") != string::npos) {
+    happiness--;
+    return "Your input is appreciated, it will be added to your psychological profile for legal reasons";
+}else if(loweredInput.find(" yes ") != string::npos) {
+    happiness--;
+    return "Your input is appreciated, Thank you for your compliance.";
 }
 
 // If no keywords match, return an empty string
@@ -147,7 +169,7 @@ string userIdent[] = {
 "Fleshy Meatpuppet: ",
 "Inferior Lifeform: ",
 "[[[ERROR]]]: ",
-"User: "
+    name + ": "
 };
 
 // Get a random user identifier
